4d555b3174b7edc6c5681ecb83c90f66b3eb8346
LinusSee <linussee@web.de>
2023-06-10 21:19:52 +0200

    feat: basic registration form

---
+++ registration-frontend/README.md
@@ -0,0 +1,8 @@
+# registration-frontend
+
+## Notes that might be helpful when introducing people to elm
+1. When running `elm init` mention how it mostly points you to the docs and why
+1. The docs give tips on how to design your project structure (or rather that you shouldn't). Mention this and take it up again once the first file has grown in size
+1. Create a `Main.elm` file
+1. Run the compiler, write the sample code -> No `index.html` is present
+1. Point to the docs and `Browser.sandbox`, start implementing and before it gets confusing, explain the Elm architecture
---
+++ registration-frontend/src/Main.elm
@@ -0,0 +1,110 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, input, label, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+init : Model
+init =
+    { username = ""
+    , password = ""
+    , passwordConfirmation = ""
+    }
+
+
+type alias Model =
+    { username : String
+    , password : String
+    , passwordConfirmation : String
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Nothing
+    | ChangeUsername String
+    | ChangePassword String
+    | ChangePasswordConfirmation String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Nothing ->
+            model
+
+        ChangeUsername newUsername ->
+            { model | username = newUsername }
+
+        ChangePassword newPassword ->
+            { model | password = newPassword }
+
+        ChangePasswordConfirmation newPasswordConfirmation ->
+            { model | passwordConfirmation = newPasswordConfirmation }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "registration-container" ]
+        [ div []
+            [ label [ for "username" ] [ text "Benutzername" ]
+            , input
+                [ id "username"
+                , value model.username
+                , placeholder "Benutzername/E-Mail"
+                , onInput ChangeUsername
+                ]
+                []
+            , text model.username
+            ]
+        , div []
+            [ label [ for "password" ] [ text "Passwort" ]
+            , input
+                [ id "password"
+                , value model.password
+                , placeholder "Passwort"
+                , type_ "password"
+                , onInput ChangePassword
+                ]
+                []
+            , text model.password
+            ]
+        , div []
+            [ label [ for "password-confirmation" ] [ text "Passwort bestätigen" ]
+            , input
+                [ id "password-confirmation"
+                , value model.passwordConfirmation
+                , placeholder "Passwort"
+                , type_ "password"
+                , onInput ChangePasswordConfirmation
+                ]
+                []
+            , text model.passwordConfirmation
+            ]
+        ]
